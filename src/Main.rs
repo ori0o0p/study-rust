ffed7cf6e439b3112bb46a4817b337b845bf97a3
101 <tmddnjs001133@dsm.hs.kr>
2024-04-26 20:18:06 +0900

    add :: examples

--- src/Main.rs
+++ src/Main.rs
@@ -1,4 +1,12 @@
 fn main() {
-    println!("hello rust!")
+    println!("{}", integer_sum(05, 11));
+    println!("{}", string_sum("민", "주"));
+}
 
+fn integer_sum(a: i32, b: i32) -> i32 {
+    return a + b
+}
+
+fn string_sum(a: &str, b: &str) -> String {
+    return a.to_owned() + &*b
 }

22e2fe85c44dbc036f9e1b2714f3aff2455613cb
101 <tmddnjs001133@dsm.hs.kr>
2024-04-26 20:26:09 +0900

    add :: examples

--- src/Main.rs
+++ src/Main.rs
@@ -1,6 +1,23 @@
+use std::cmp::min;
+
 fn main() {
-    println!("{}", integer_sum(05, 11));
-    println!("{}", string_sum("민", "주"));
+    let mut min_ju = 1;
+    min_ju += 100;
+
+    println!("{}", integer_sum(min_ju, 11));
+
+    let min_ju = "민주";
+    println!("{}", string_sum("박", min_ju));
+}
+
+fn scope() {
+    let a = 1;
+    {
+        let a = "아일릿";
+        println!("{}", a);
+    }
+
+    println!("{}", a);
 }
 
 fn integer_sum(a: i32, b: i32) -> i32 {

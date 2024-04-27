e728e0e6471e9f4e992c30530a25ed0800639788
101 <tmddnjs001133@dsm.hs.kr>
2024-04-27 21:36:38 +0900

    update :: Main.rs
    add guess_number_game

--- src/Main.rs
+++ src/Main.rs
@@ -1,4 +1,46 @@
+use std::io;
+use rand::{random, Rng};
+
 fn main() {
+    guess_number_game()
+}
+
+fn guess_number_game() {
+    println!("숫자를 맞춰보세요!");
+
+    let rand_int = rand::thread_rng()
+        .gen_range(1..=10);
+
+    loop {
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("읽기 실패");
+
+        let input: i32 = input.trim()
+            .parse()
+            .expect("읽기 실패");
+
+        let mut result: bool = rand_int.eq(&input);
+
+        if result {
+            println!("\n맞추셨습니다!!");
+        } else {
+            println!("\n틀리셨습니다..");
+        }
+
+        println!("입력하신 숫자 : {}", input);
+        println!("컴퓨터가 생각한 숫자 : {}", rand_int);
+
+        if result {
+            break;
+        }
+    }
+
+}
+
+fn mutable_variable() {
     let mut min_ju = 1;
     min_ju += 100;
 

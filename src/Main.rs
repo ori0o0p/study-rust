50bb9fa5d7343063f851caea31aa9a4a1e80ef74
101 <tmddnjs001133@dsm.hs.kr>
2024-04-28 09:51:48 +0900

    update :: Main.rs
    러스트 공부

--- src/Main.rs
+++ src/Main.rs
@@ -2,7 +2,63 @@ use std::io;
 use rand::{random, Rng};
 
 fn main() {
-    guess_number_game()
+    expression()
+}
+
+fn expression() {
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("{}", y);
+}
+
+fn tuple() {
+    let tuple: (i32, u32, &str, f64) = (-10, 100, "아일릿", 1.2);
+
+    println!("{}, {}, {}, {}", tuple.0, tuple.1, tuple.2, tuple.3);
+
+    let (a, b, c, d) = tuple;
+
+    println!("{}, {}, {}, {}", a, b, c, d);
+}
+
+fn array() {
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+
+    a.iter().for_each(|&value| println!("{}", value));
+}
+
+fn baek_joon_11021() {
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("읽기 실패");
+
+    let input: i32 = input.trim()
+        .parse()
+        .expect("읽기 실패");
+
+    for a in (1..input + 1) {
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("읽기 실패");
+
+        let input = input.trim();
+
+        let numbers: Vec<i32> = input
+            .split_whitespace()
+            .map(|s| s.parse().expect("숫자로 변환 실패"))
+            .collect();
+
+        let sum: i32 = numbers.iter().sum();
+
+        println!("Case #{}: {}", a, sum);
+    }
 }
 
 fn guess_number_game() {
